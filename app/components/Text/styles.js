9eb8da97479967d93ab27d072b1e0bd37aa5ef59
Cody Craig <cody.craig@aol.com>
2019-05-09 16:45:53 -0400

    Add Header, Button, and styled Text.

---
+++ app/components/Text/styles.js
@@ -0,0 +1,11 @@
+import EstyleSheet from 'react-native-extended-stylesheet';
+
+const styles = EstyleSheet.create({
+  smallText: {
+    color: '$white',
+    fontSize: 12,
+    textAlign: 'center'
+  }
+});
+
+export default styles;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,13 +4,19 @@ import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Buttons'
+import { LastConverted } from '../components/Text';
+import { Header } from '../components/Header';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends React.Component {
+
   handlePressBaseCurrency = () => {
     console.log('press base');
   };
@@ -20,14 +26,23 @@ class Home extends React.Component {
   };
 
   handleTextChange = (text) => {
-    console.log('change text', text)
+    console.log('change text', text);
+  };
+
+  handleSwapCurrency = () => {
+    console.log('press swap currency');
+  };
+
+  handleOptionsPress = () => {
+    console.log("Handle options press");
   };
 
   render() {
     return (
         <Container>
           <StatusBar translucent={false} barStyle="light-content" />
-          <Logo />
+          <Header onPress={this.handleOptionsPress}/>
+          <Logo/>
           <InputWithButton
               buttonText={TEMP_BASE_CURRENCY}
               onPress={this.handlePressBaseCurrency}
@@ -40,6 +55,16 @@ class Home extends React.Component {
               onPress={this.handlePressQuoteCurrency}
               editable={false}
               value={TEMP_QUOTE_PRICE}
+          />
+          <LastConverted
+            base={TEMP_BASE_CURRENCY}
+            quote={TEMP_QUOTE_CURRENCY}
+            date={TEMP_CONVERSION_DATE}
+            conversionRate={TEMP_CONVERSION_RATE}
+          />
+          <ClearButton
+            text="Reverse Currencies"
+            onPress={this.handleSwapCurrency}
           />
         </Container>
     );
